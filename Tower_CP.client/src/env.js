f1c582a1098e8d1b81a6554fd9fe6be7611b23c7
Kyle Evans <krevan88@protonmail.com>
2022-01-28 14:33:20 -0700

    env update

--- Tower_CP.client/src/env.js
+++ Tower_CP.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-hgqjrro9.us.auth0.com'
+export const clientId = 'JrqqXscucubFtUlHT7GxKeNrn0rvmBXv'
+export const audience = 'https://gregslist.com'
